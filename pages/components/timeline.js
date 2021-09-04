3d4a59ccd56e1e61e757e1eaecdc96f8b3febaae
majasmee <ameen_aic@hotmail.com>
2021-09-05 00:18:31 +0700

    Design UI

---
+++ pages/components/timeline.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Timeline = () => {
+    return (
+        <div className="card card-body" style={{ backgroundColor: "#ffc107"}}>
+            <h4 className="text-center text-dark mb-3">Timeline</h4>
+            <div className="row">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Timeline
